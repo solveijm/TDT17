16671ac5b2c0cba7a0a90426bca2e427b09bf033
Andrea Jessen <andrea_jes@hotmail.com>
2022-11-21 11:36:44 +0100

    Small fix in slurm

--- yolov5_train.py
+++ yolov5_train.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("python ./yolov5/train.py --img 640 --batch 16 --epochs 300 --data coco128.yaml --weights yolov5s.pt --device 0")
+os.system("python ./yolov5/train.py --img 640 --batch 32 --epochs 300 --data dataset.yaml --weights yolov5x.pt --device 0")
